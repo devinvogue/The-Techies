85c185fb9c566004472784d0ea87b4491cc05034
adjoaEdwin <aedwin905@gmail.com>
2018-02-20 16:33:04 +0000

    Abeg, save this for me

---
+++ assets/js/feedback.js
@@ -0,0 +1,5 @@
+var alertButton= document.getElementById('alertButton');
+function sayLetsMoveTogether() {
+	alert("Let's Move Together")
+}
+alertButton.addEventListener("click", sayLetsMoveTogether);
