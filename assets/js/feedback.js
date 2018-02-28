c129fa859ff1672e698523171511fd03e484e6e7
Maxwell <geek@max-dex.com>
2018-02-28 11:38:54 +0000

    final work

--- assets/js/feedback.js
+++
@@ -1,5 +0,0 @@
-var alertButton= document.getElementById('alertButton');
-function sayLetsMoveTogether() {
-	alert("Let's Move Together")
-}
-alertButton.addEventListener("click", sayLetsMoveTogether);
